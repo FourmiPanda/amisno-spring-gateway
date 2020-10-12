25c7d9fd824da76dc447a94161ccb07bfc3b9831
Ismail El Khantache <ismail.elkhantache@amplexor.com>
2020-10-12 16:28:36 +0200

    Initial commit

---
+++ src/main/java/com/amisno/infrazuul/InfraZuulApplication.java
@@ -0,0 +1,13 @@
+package com.amisno.infrazuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraZuulApplication.class, args);
+    }
+
+}
