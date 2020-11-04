36e4eee5b9f5fc05959d9e742a57e7fba0515415
Ismail EL KHANTACHE <ismail.elkhantache@amplexor.com>
2020-11-05 00:17:31 +0100

    Initial commit

---
+++ src/test/java/com/amisno/infragateway/InfraGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amisno.infragateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
