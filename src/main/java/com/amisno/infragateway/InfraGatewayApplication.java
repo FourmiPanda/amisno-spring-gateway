5d0d7560f62451ad0326869f8c219ef2e1206d40
Ismail EL KHANTACHE <ismail.elkhantache@amplexor.com>
2020-11-07 00:21:54 +0100

    Implement eureka client

--- src/main/java/com/amisno/infragateway/InfraGatewayApplication.java
+++ src/main/java/com/amisno/infragateway/InfraGatewayApplication.java
@@ -2,8 +2,10 @@ package com.amisno.infragateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InfraGatewayApplication {
 
 	public static void main(String[] args) {
